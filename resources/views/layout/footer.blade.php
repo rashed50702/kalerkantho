ffbc4f84e9e75262b00d549f7de02a4e6ce73dbb
Md. Rashedul Hasan <rashed50702@gmail.com>
2023-05-08 17:30:18 +0600

    first commit

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,89 @@
+<footer class="footer-section mt-5 bg-light">
+    <div class="container">
+        <div class="footer-content pt-5">
+            <div class="row">
+                <div class="col-md-6 col-xl-3 col-lg-3 mb-5">
+                    <div class="footer-widget">
+                        <div class="footer-text">
+                            <div class="footer-widget footer-app">
+                                <div class="footer-widget-heading">
+                                    <h3>মোবাইল অ্যাপস ডাউনলোড করুন</h3>
+                                </div>
+                                <ul>
+                                    <li><a target="_blank" href="https://play.google.com/store/apps/details?id=com.kalerkantho&amp;hl=bn&amp;gl=US"><img alt="" loading="lazy" width="160" height="58" decoding="async" data-nimg="1" class="w-75 h-auto" style="color:transparent" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fandroid.22a14a78.png&amp;w=256&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fandroid.22a14a78.png&amp;w=384&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fandroid.22a14a78.png&amp;w=384&amp;q=75"></a></li>
+                                    <li><a target="_blank" href="https://apps.apple.com/us/app/kaler-kantho/id1067229620"><img alt="" loading="lazy" width="160" height="58" decoding="async" data-nimg="1" class="w-75 h-auto" style="color:transparent" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FIos-app.0c819c2e.png&amp;w=256&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2FIos-app.0c819c2e.png&amp;w=384&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FIos-app.0c819c2e.png&amp;w=384&amp;q=75"></a></li>
+                                </ul>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="footer-social-icon mt-4 d-block"><span>অনুসরণ করুন</span><a target="_blank" href="https://www.facebook.com/kalerkantho"><i class="bi bi-facebook"></i></a><a target="_blank" href="https://twitter.com/theKalerKantho"><i class="bi bi-twitter"></i></a><a target="_blank" href="https://www.instagram.com/thekalerkantho"><i class="bi bi-instagram"></i></a><a target="_blank" href="https://www.youtube.com/channel/UCsBqX8QguRdkawdlNbIL1dw"><i class="bi bi-youtube"></i></a></div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-4 col-lg-4 col-md-5 mb-30">
+                    <div class="footer-widget">
+                        <div class="footer-widget-heading">
+                            <h3>আজকের পত্রিকা</h3>
+                        </div>
+                        <ul>
+                            <li><a href="/print-edition/first-page">প্রথম পাতা</a></li>
+                            <li><a href="/print-edition/last-page">শেষের পাতা</a></li>
+                            <li><a href="/[edition]/print-edition/sports">খেলা</a></li>
+                            <li><a href="/print-edition/news">খবর</a></li>
+                            <li><a href="/online/shuvosangho">শুভসংঘ</a></li>
+                            <li><a href="/print-edition/deshe-deshe">দেশে দেশে</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-3 col-lg-3 col-md-4 mb-50">
+                    <div class="footer-widget">
+                        <div class="footer-widget-heading">
+                            <h3>অনলাইন</h3>
+                        </div>
+                        <ul>
+                            <li><a href="/online/national">জাতীয়</a></li>
+                            <li><a href="/online/country-news">সারাবাংলা</a></li>
+                            <li><a href="/online/world">বিশ্ব</a></li>
+                            <li><a href="/online/business">বাণিজ্য</a></li>
+                            <li><a href="/online/entertainment">বিনোদন</a></li>
+                            <li><a href="/online/sport">খেলাধুলা</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-2 col-lg-2 col-md-3 mb-5">
+                    <div class="footer-widget">
+                        <div class="footer-widget-heading">
+                            <h3>বিজ্ঞাপন</h3>
+                        </div>
+                        <ul>
+                            <li class="d-block float-none w-100"><a target="_blank" href="/price-list-print-version.jpg">মূল্য তালিকা (প্রিন্ট ভার্সন)</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyright-area bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-center text-lg-left">
+                    <div class="copyright-text top-txt">
+                        <p><b>প্রধান সম্পাদক : ইমদাদুল হক মিলন ;</b> সম্পাদক: শাহেদ মুহাম্মদ আলী</p>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-right">
+                    <div class="footer-menu">
+                        <ul>
+                            <li><a href="/about-us">আমাদের সম্পর্কে</a></li>
+                            <li><a href="/privacy-policy">শর্তাবলী</a></li>
+                            <li><a href="/privacy-policy">গোপনীয়তা নীতি</a></li>
+                            <li><a href="/contact-us">যোগাযোগ করুন</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="copyright-text">
+                    <p class="text-center mt-4">স্বত্ব © <!-- -->২০২৩<!-- --> কালের কণ্ঠ</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\FrontEndController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [FrontEndController::class, 'index']);
+
+
